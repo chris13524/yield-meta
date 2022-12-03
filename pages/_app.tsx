434e8abb47e2a322934a2c824045732552c727f4
Chris Smith <chris@smith.xyz>
2022-12-03 12:30:02 +0530

    Add RainbowKit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,47 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  ConnectButton,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+
+const { chains, provider } = configureChains(
+  [
+    // chain.mainnet,
+    // chain.polygon,
+    chain.polygonMumbai,
+  ],
+  [
+    alchemyProvider({ apiKey: "E0obo9iM1x_92GbGIKj4ZY0i96aFne9M" }),
+    // publicProvider(),
+  ],
+);
+const { connectors } = getDefaultWallets({
+  appName: 'Yield Meta',
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ConnectButton />
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
